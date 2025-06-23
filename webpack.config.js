74d2c1e5c84f982dd8905fb6fd2d9eac96ab340d
samu <varjao.samueloliveira@gmail.com>
2025-06-23 16:46:57 -0300

    Ajuste na versão mobile

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'public'),
+    clean: true,
   },
   module: {
     rules: [
@@ -19,13 +22,36 @@ module.exports = {
         test: /\.css$/,   // <-- Adicione isso para o CSS funcionar
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/i,
+        use: [{
+          loader: 'url-loader',
+          options: { limit: 10000 }
+        }]
+      },
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  devServer: {
-    static: path.join(__dirname, 'public'),
-    port: 8080,
+devServer: {
+  static: {
+    directory: path.resolve(__dirname, 'public'),
+    publicPath: '/',
   },
+},
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', globOptions: { ignore: ['**/index.html'] } }
+      ]
+    }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
 };
+
+
