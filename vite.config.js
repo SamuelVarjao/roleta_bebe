26f6bd0bd6da6f8453a3d207f8e26636ddf3850f
samuelv2umc <11221103351@alunos.umc.br>
2026-01-16 20:03:28 -0300

    deploy corrigido com base do Vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/roleta_bebe/',   // ← AQUI é o lugar certo
   plugins: [
     react({
       babel: {
-        base: 'https://github.com/SamuelVarjao/roleta_bebe.git',
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
