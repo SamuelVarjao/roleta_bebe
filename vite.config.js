2ad4c9201c6f45aca5a4e81d02809f3e2227800f
samuelv2umc <11221103351@alunos.umc.br>
2026-01-18 21:36:10 -0300

    ajuste de base do Vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/roleta_bebe/',   // ← AQUI é o lugar certo
+  base: '/',   // ← AQUI é o lugar certo
   plugins: [
     react({
       babel: {
