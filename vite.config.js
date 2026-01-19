cc68313beab8043ec55bfff7c6d2f81e47106f8a
samuelv2umc <11221103351@alunos.umc.br>
2026-01-18 21:42:08 -0300

    corrige base do Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',   // ← AQUI é o lugar certo
+  base: '/roleta_bebe/',   
   plugins: [
     react({
       babel: {
